a1aecf8d359f08a399ff2af143d2a37067c922a3
thekirboking <jonathan.chalifoux@outlook.com>
2025-04-03 12:06:51 -0400

    create repo

--- src/Main/java/GamePanel.java
+++ src/Main/java/GamePanel.java
@@ -7,7 +7,7 @@ public class GamePanel extends JPanel { // GamePanel class extends JPanel to cre
 
     //screen settings
     final int originalTileSize = 16; // Original tile size in pixels
-    final int scale = 3; // how much the caracter needs to be scale bc 16 by 16 is too small
+    final int scale = 3; // how much the character needs to be scale bc 16 by 16 is too small
 
     final int tileSize = originalTileSize * scale; // Final tile size after scaling
     final int maxScreenCol = 16; // Maximum number of columns on the screen
@@ -15,10 +15,21 @@ public class GamePanel extends JPanel { // GamePanel class extends JPanel to cre
     final int screenWidth = tileSize * maxScreenCol; // Width of the screen in pixels
     final int screenHeight = tileSize * maxScreenRow; // Height of the screen in pixels
 
+    Thread gameThread; // Thread for running the game loop
+
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight)); // Set the preferred size of the panel
         this.setBackground(Color.black); // Set the background color of the panel to black
         this.setDoubleBuffered(true); // Enable double buffering for smoother rendering
     }
+
+    public void startGameThread() {
+        gameThread = new Thread(this::startGameThread); // Create a new thread for the game
+
+
+    }
+
+
+
 }
